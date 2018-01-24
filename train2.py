d31234691a1842c306afb74b41265308a4958397
zjc798719285 <798719285@qq.com>
2018-01-24 09:01:12 +0800

    placeholder style have been changed

--- train2.py
+++ train2.py
@@ -9,16 +9,16 @@ import math
 def train(img, ground_truth,test_img, test_gt, model, params):
     # placeholder define
     Image = tf.placeholder(tf.float32, params.batch_shape)
-    PosNegRoi = tf.placeholder(tf.float32, [params.num_rois, 4])
-    GroundTruthRoi = tf.placeholder(tf.float32, [params.num_rois, 4])
+    ClsRoi = tf.placeholder(tf.float32, [params.num_rois, 4])
+    ClsGtRoi = tf.placeholder(tf.float32, [params.num_rois, 4])
     ClsLabel = tf.placeholder(tf.float32, [params.num_rois, 2])
 
    # ConvNet define and loss calculation
     base_net = model.base_net(x=Image, trainable=True)
-    cls_label = model.classcify(base_net=base_net, rois=PosNegRoi, out_size=params.roi_shape, trainable=True)
-    box = model.box_regressor(base_net=base_net, rois=PosNegRoi, out_size=params.roi_shape, trainable=True)
+    cls_label = model.classcify(base_net=base_net, rois=ClsRoi, out_size=params.roi_shape, trainable=True)
+    box = model.box_regressor(base_net=base_net, rois=ClsRoi, out_size=params.roi_shape, trainable=True)
     cls_loss = loss_classify(cls_predic=cls_label, labels=ClsLabel)
-    roi_loss = loss_box_regressor(gt=GroundTruthRoi, dr=box, mode=params.box_loss)
+    roi_loss = loss_box_regressor(gt=ClsGtRoi, dr=box, mode=params.box_loss)
     opt1 = tf.train.AdadeltaOptimizer(learning_rate=params.learning_rate, rho=params.rho)
     loss = cls_loss + params.loss_balance*roi_loss
     loss_opt = opt1.minimize(loss)
@@ -39,8 +39,8 @@ def train(img, ground_truth,test_img, test_gt, model, params):
                 img[0, :, :, :] = train_x[step, :, :, :]
                 rois, cls_label = pos_neg_roi_generator(train_roi[step], int(params.num_rois/2))
                 gt_box_roi = box_roi_generator(cls_label=cls_label, roi=train_roi[step])
-                sess.run(loss_opt, feed_dict={Image: img, GroundTruthRoi: gt_box_roi,
-                                               PosNegRoi: rois, ClsLabel: cls_label})
+                sess.run(loss_opt, feed_dict={Image: img, ClsGtRoi: gt_box_roi,
+                                              ClsRoi: rois, ClsLabel: cls_label})
                 # if step%500 == 0:
                 #    iou = sess.run(iou, feed_dict={Image: img, GroundTruthRoi: gt_box_roi,
                 #                                   PosNegRoi: rois, ClsLabel: cls_label})
